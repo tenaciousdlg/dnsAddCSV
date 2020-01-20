9d2570c92d4e0b1a1ad152f71db3ee763319070e
Chris De La Garza <cdelagarza@cloudflare.com>
2020-01-20 15:53:43 -0600

    Updated with command line support

--- dnsAddCSV.py
+++ dnsAddCSV.py
@@ -6,26 +6,33 @@
 # A,tom.atxflare.cf.,1.1.1.1
 # CNAME,jerry.atxflare.cf,mouse.example.com
 #-----------------------------------------
-
 import csv
 import CloudFlare
+import argparse
+
+parser = argparse.ArgumentParser(description='Flag and Usage Help')
+parser.add_argument("-i", default=None, required=True, type=str, help="Relative location of CSV file. Example: -i ./test.csv")
+parser.add_argument("-z", default=None, required=True, type=str, help="Name of Cloudflare domain such as example.com")
+args = parser.parse_args()
+
+file = args.i
+zone = args.z
 
 values = []
-with open('./test.csv') as f:
+with open(file) as f:
     reader = csv.reader(f)
     for row in reader:
         values.append(row)
 
 keys = ['type', 'name', 'content']
 dnsRecords = []
-
 for value in values:
     dnsRecords.append(dict(zip(keys, value)))
 
-zone_name = 'example.com'
+zone_name = zone
 cf = CloudFlare.CloudFlare()
-zone = cf.zones.get(params={'zone_name': "[domain]",'per_page': 1})
-zone_id = zone[0]["id"]
+zone_data = cf.zones.get(params={'zone_name': "[domain]",'per_page': 1})
+zone_id = zone_data[0]["id"]
 
 for record in dnsRecords:
     r = cf.zones.dns_records.post(zone_id, data = record)
